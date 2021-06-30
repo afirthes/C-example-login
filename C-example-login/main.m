bcc598c35764924a137c55bfa86118ff78af6285
windwail <windwail>
2021-06-30 10:11:18 +0300

    added code

--- C-example-login/main.m
+++ C-example-login/main.m
@@ -1,16 +1,32 @@
-//
-//  main.m
-//  C-example-login
-//
-//  Created by Sergio on 30.06.2021.
-//
 
-#import <Foundation/Foundation.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef  struct {
+  char *name;
+  size_t age;
+} person_t;
+
+void login_user(person_t *user);
 
 int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
-  }
+  person_t user;
+  login_user(&user);
   return 0;
+}
+
+void login_user(person_t *user) {
+  char buffer[256];
+  
+  puts("Login: ");
+  scanf("%s", buffer);
+  printf("Hello, %s \n", buffer);
+  
+  user->name = malloc(sizeof(char) * strlen(buffer) + 1);
+  user->name = strcpy(user->name, buffer);
+  
+  puts("Age: ");
+  scanf("%ul", &user->age);
+  printf("Nice, %lu \n", user->age);
 }
